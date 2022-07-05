226e880cf32385f5c629f680a82cc408f0679205
Mar4ik_ <brainocswell@gmail.com>
2022-07-05 17:03:22 +0300

    Header Navebar Profile CSS

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+            <header className='header'>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiHWGjDJVyCQ-TNbBS0qBRYADozUMmhiirLw&usqp=CAU" alt="logo"/>
+            </header>
+    );
+};
+
+
+
+export default Header;
---
+++ my-app/src/components/Profile.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+const Profile = () => {
+    return (
+        <div>
+            <div className='content'>
+                <div >
+                    <img src="https://images.unsplash.com/photo-1553615738-d8e0829f1d61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8N3x8fGVufDB8fHx8&w=1000&q=80" alt="foto-main"/>
+                </div>
+                <div>
+                    ava+Status
+                </div>
+                <div>
+                    My posts
+                </div>
+                <div>post1</div>
+                <div>post2</div>
+                <div>post3</div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
