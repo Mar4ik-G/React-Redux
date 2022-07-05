07c6758b820884a821ae2fb4863a1d9ff81dbbe8
Mar4ik_ <brainocswell@gmail.com>
2022-07-05 20:48:13 +0300

    Create Post CSS

--- my-app/src/components/Profile/MyPosts/MyPosts.js
+++ my-app/src/components/Profile/MyPosts/MyPosts.js
@@ -5,11 +5,14 @@ import MyPost from "./Post/MyPost";
 const MyPosts = () => {
     return (
         <div className={MPStyle.block}>
-            <div>My posts</div>
-            <div><textarea name="" id="" cols="30" rows="3"></textarea></div>
-            <button>Add Post</button>
-            <MyPost/>
-
+            <div className={MPStyle.HText}>New Post</div>
+            <div className={MPStyle.formik}>
+                <div><input className={MPStyle.input} type="text"/></div>
+                <button className={MPStyle.button}>Add Post</button>
+            </div>
+            <MyPost text={"Hi"} like={"1"}/>
+            <MyPost text={"Where are you from?"} like={"23"}/>
+            <MyPost text={"Hi my name Mark"} like={"27"}/>
         </div>
     );
 };
--- my-app/src/components/Profile/MyPosts/Post/MyPost.js
+++ my-app/src/components/Profile/MyPosts/Post/MyPost.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import MPostStyle from "../Post/MPostStyle.module.css";
 
-const MyPost = () => {
+const MyPost = (props) => {
     return (
         <div className={MPostStyle.item}>
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCF0F86bWVGKdXn52LH-L819uOvAepAuqa3_9fknbfc_Hxyqg2acuB11LbIUq66LMElmU&usqp=CAU" alt="avatar"/>
-            post1</div>
+            <p className={MPostStyle.text}>{props.text}</p>
+            <p className={MPostStyle.like}>{props.like} Like</p>
+        </div>
     );
 };
 
