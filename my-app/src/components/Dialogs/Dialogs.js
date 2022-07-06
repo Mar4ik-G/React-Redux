ee05be5817e7dd56a13937ac97cf8efb04c33024
Mar4ik_ <brainocswell@gmail.com>
2022-07-06 08:54:42 +0300

    page dialogs

--- my-app/src/components/Dialogs/Dialogs.js
+++ my-app/src/components/Dialogs/Dialogs.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import DStyle from "./Dialogs.module.css"
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () => {
     return (
         <div className={DStyle.content}>
-111
+            <div className={DStyle.dialogsItems}>
+                <div className={DStyle.dialog}><NavLink to='/dialogs/1'>Mark</NavLink></div>
+                <div className={DStyle.dialog}>  <NavLink to='/dialogs/2'>Andrey</NavLink></div>
+                <div className={DStyle.dialog}>  <NavLink to='/dialogs/3'>Oleg</NavLink></div>
+                <div className={DStyle.dialog}>  <NavLink to='/dialogs/4'>Dima</NavLink></div>
+                <div className={DStyle.dialog}>  <NavLink to='/dialogs/5'>Mark</NavLink></div>
+            </div>
+            <div className={DStyle.messages}>
+                <div className={DStyle.message}>ni</div>
+                <div className={DStyle.message}>OKK</div>
+                <div className={DStyle.message}>QWERTY</div>
+            </div>
         </div>
     );
 };
