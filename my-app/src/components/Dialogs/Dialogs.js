54ecd530f8879709ed87b84f5bfb4309c37f67a5
Mar4ik_ <brainocswell@gmail.com>
2022-07-06 10:23:13 +0300

    map data bll ui

--- my-app/src/components/Dialogs/Dialogs.js
+++ my-app/src/components/Dialogs/Dialogs.js
@@ -5,21 +5,34 @@ import DialogMessage from "./DialogMassege/DialogMessage";
 
 
 const Dialogs = () => {
+
+    let dialogsItem =[
+        {name:'Mark', id:'1'},
+        {name:'Andrey', id:'2'},
+        {name:'Oleg', id:'3'},
+        {name:'Dima', id:'4'},
+        {name:'Mark', id:'5'},
+        {name:'Oleg', id:'6'},
+    ];
+
+    let messagesData =[
+        {message:'Mark', id:'1'},
+        {message:'Andrey', id:'2'},
+        {message:'Oleg', id:'3'},
+        {message:'Dima', id:'4'},
+        {message:'Mark', id:'5'},
+        {message:'Oleg', id:'6'},
+    ];
+
+
     return (
         <div className={DStyle.content}>
             <div className={DStyle.dialogsItems}>
-               <DialogItem name='Mark' id='1'/>
-               <DialogItem name='Andrey' id='2'/>
-               <DialogItem name='Oleg' id='3'/>
-               <DialogItem name='Dima' id='4'/>
-               <DialogItem name='Mark' id='5'/>
-               <DialogItem name='Oleg' id='6'/>
+                {dialogsItem.map(e => <DialogItem name={e.name} id={e.id} key={e.id}/>)}
             </div>
             <div className={DStyle.verticalLine}></div>
             <div className={DStyle.messages}>
-               <DialogMessage message='Hi'/>
-               <DialogMessage message='OKKK'/>
-               <DialogMessage message='HELLO'/>
+                {messagesData.map(e => <DialogMessage message={e.message} key={e.id}/>)}
 
             </div>
         </div>
--- my-app/src/components/Profile/MyPosts/MyPosts.js
+++ my-app/src/components/Profile/MyPosts/MyPosts.js
@@ -2,7 +2,17 @@ import React from 'react';
 import MPStyle from "../MyPosts/MPS.module.css";
 import MyPost from "./Post/MyPost";
 
+
 const MyPosts = () => {
+    let messagesData =[
+        {like:13,message:'Mark', id:'1'},
+        {like:21,message:'Where are you from?', id:'2'},
+        {like:41,message:'name Mark', id:'3'},
+        {like:15,message:'Hi my name Mark', id:'4'},
+        {like:23,message:'Where are you from?', id:'5'},
+        {like:54,message:'Hi my name Mark', id:'6'},
+    ];
+
     return (
         <div className={MPStyle.block}>
             <div className={MPStyle.HText}>New Post</div>
@@ -10,9 +20,7 @@ const MyPosts = () => {
                 <div><input className={MPStyle.input} type="text"/></div>
                 <button className={MPStyle.button}>Add Post</button>
             </div>
-            <MyPost text={"Hi"} like={"1"}/>
-            <MyPost text={"Where are you from?"} like={"23"}/>
-            <MyPost text={"Hi my name Mark"} like={"27"}/>
+            {messagesData.map(e => <MyPost text={e.message} like={e.like} key={e.id}/>)}
         </div>
     );
 };
