337c9a5a3c7dfcd85e8d028dcedbdf7af0aa2f3c
Mar4ik_ <brainocswell@gmail.com>
2022-07-06 17:02:41 +0300

    Ref OnClick etc

--- my-app/src/components/Dialogs/Dialogs.js
+++ my-app/src/components/Dialogs/Dialogs.js
@@ -3,7 +3,16 @@ import DStyle from "./Dialogs.module.css"
 import DialogItem from "./DialogItem/DialogItem";
 import DialogMessage from "./DialogMassege/DialogMessage";
 
+
 const Dialogs = (props) => {
+
+    const addMessageRef = React.createRef();
+
+    const addMessage = () => {
+        let text = addMessageRef.current.value
+        alert(text)
+    }
+
     return (
         <div className={DStyle.content}>
             <div className={DStyle.dialogsItems}>
@@ -12,6 +21,10 @@ const Dialogs = (props) => {
             <div className={DStyle.verticalLine}></div>
             <div className={DStyle.messages}>
                 {props.dialogsProps.messageData.map(e => <DialogMessage message={e.message} key={e.id}/>)}
+               <div className={DStyle.inpBtn}>
+                   <input type="text" ref={addMessageRef} className={DStyle.input}/>
+                   <button className={DStyle.button} onClick={addMessage}>Sent message</button>
+               </div>
             </div>
         </div>
     );
