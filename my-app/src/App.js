d618046823ed523d74fa317e2205f91bd183d4c8
Mar4ik_ <brainocswell@gmail.com>
2022-07-16 12:40:04 +0300

    React lazy Suspense

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,9 +2,7 @@ import './App.css';
 import React,{ Suspense } from 'react';
 import Navbar from "./components/Navbar/Navbar";
 import {Route, Routes} from "react-router-dom";
-import NewsContainer from "./components/News/NewsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
-// import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./components/Login/Login";
 import Preloader from "./components/Preloader/Preloader";
@@ -12,6 +10,7 @@ import Preloader from "./components/Preloader/Preloader";
 
 const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
 const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
+const NewsContainer = React.lazy(() => import('./components/News/NewsContainer'));
 
 
 function App() {
@@ -23,7 +22,6 @@ function App() {
             <div className="content">
                 <Suspense fallback={<Preloader/>}>
                 <Routes>
-
                         <Route path="/profile/:userId" element={<ProfileContainer/>}/>
                         <Route path="/dialogs/*" element={<DialogsContainer/>}/>
                         <Route path='/profile' element={<ProfileContainer/>}/>
